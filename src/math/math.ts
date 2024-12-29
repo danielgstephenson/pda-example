049a691f84591b47c99afbe3b423ed6763ffec15
dgstephenson <dgstephenson.econ@gmail.com>
2024-12-28 23:19:22 -0500

    clip

--- src/math/math.ts
+++ src/math/math.ts
@@ -1,14 +1,14 @@
 import { Vec2 } from './vec2'
 import { Vec3 } from './vec3'
 
-const sqRt3 = Math.sqrt(3)
+export const sqrt3 = Math.sqrt(3)
 
 export function simplexToBox (v: Vec3): Vec2 {
-  return new Vec2(0.5 * v.z + v.y, 0.5 * sqRt3 * v.z)
+  return new Vec2(0.5 * v.z + v.y, 0.5 * sqrt3 * v.z)
 }
 
 export function boxToSimplex (v: Vec2): Vec3 {
-  const z = 2 / sqRt3 * v.y
+  const z = 2 / sqrt3 * v.y
   const y = v.x - 0.5 * z
   const x = 1 - y - z
   return new Vec3(x, y, z)
--- src/triangle.ts
+++ src/triangle.ts
@@ -1,8 +1,8 @@
-import { boxToSimplex } from './math/math'
+import { boxToSimplex, sqrt3 } from './math/math'
 import { Vec2 } from './math/vec2'
 
 export class Triangle {
-  static resolution = 100
+  static resolution = 300
   canvas: HTMLCanvasElement
   context: CanvasRenderingContext2D
   imageData: ImageData
@@ -42,7 +42,21 @@ export class Triangle {
   draw (): void {
     this.resetContext()
     this.offscreenContext.putImageData(this.imageData, 0, 0)
-    this.context.drawImage(this.offscreenCanvas, 0.25, 0.25, 0.5, 0.5)
+    this.context.save()
+    this.context.beginPath()
+    const x = 0.25
+    const y = 0.2
+    const size = 0.5
+    const xMid = x + 0.5 * size
+    const height = 0.5 * sqrt3 * size
+    const halfPixel = 0.5 * size / Triangle.resolution
+    this.context.moveTo(x + halfPixel, y + halfPixel)
+    this.context.lineTo(x + size - halfPixel, y + halfPixel)
+    this.context.lineTo(xMid, y + height - halfPixel)
+    this.context.closePath()
+    this.context.clip()
+    this.context.drawImage(this.offscreenCanvas, x, y, size, size)
+    this.context.restore()
   }
 
   resetContext (): void {
