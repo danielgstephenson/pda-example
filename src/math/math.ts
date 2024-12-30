cc8225c30ce5c41c31c8727b78329c3a575bc341
dgstephenson <dgstephenson.econ@gmail.com>
2024-12-29 22:04:40 -0500

    triangle

--- src/math/math.ts
+++ src/math/math.ts
@@ -13,3 +13,23 @@ export function boxToSimplex (v: Vec2): Vec3 {
   const x = 1 - y - z
   return new Vec3(x, y, z)
 }
+
+export function roundSimplex (v: Vec3): Vec3 {
+  const x = round(v.x, 2)
+  const y = round(v.y, 2)
+  const z = 1 - x - y
+  return new Vec3(x, y, z)
+}
+
+export function toInnerSimplex (v: Vec3): Vec3 {
+  if (Math.min(v.x, v.y, v.z) >= 0) return v
+  const x = Math.max(0, v.x)
+  const y = Math.max(0, v.y)
+  const z = Math.max(0, v.z)
+  const sum = x + y + z
+  return new Vec3(x / sum, y / sum, z / sum)
+}
+
+export function round (x: number, digits: number): number {
+  return Number(x.toFixed(digits))
+}
--- src/triangle.ts
+++ src/triangle.ts
@@ -1,13 +1,18 @@
-import { boxToSimplex, sqrt3 } from './math/math'
+import { boxToSimplex, round, roundSimplex, simplexToBox, sqrt3, toInnerSimplex } from './math/math'
 import { Vec2 } from './math/vec2'
+import { Vec3 } from './math/vec3'
 
 export class Triangle {
-  static resolution = 300
+  static resolution = 100
   canvas: HTMLCanvasElement
   context: CanvasRenderingContext2D
   imageData: ImageData
   offscreenCanvas: OffscreenCanvas
   offscreenContext: OffscreenCanvasRenderingContext2D
+  strategy: Vec3 | null = null
+  drawX = 0.1
+  drawY = 0.1
+  drawSize = 0.8
 
   constructor (canvas: HTMLCanvasElement) {
     this.canvas = canvas
@@ -18,6 +23,7 @@ export class Triangle {
     this.imageData = this.getImageData()
     this.offscreenCanvas = new OffscreenCanvas(Triangle.resolution, Triangle.resolution)
     this.offscreenContext = this.offscreenCanvas.getContext('2d') as OffscreenCanvasRenderingContext2D
+    this.canvas.addEventListener('mousedown', (event: MouseEvent) => this.onMouseDown(event))
     this.draw()
   }
 
@@ -30,8 +36,7 @@ export class Triangle {
       const pixel = Math.floor(i / 4)
       const x = (pixel % width + 0.5) / width
       const y = (Math.floor(pixel / width) + 0.5) / height
-      const w = boxToSimplex(new Vec2(x, y))
-      if (Math.min(w.x, w.y, w.z) < 0) return
+      const w = toInnerSimplex(boxToSimplex(new Vec2(x, y)))
       imageArray[i + 0] = 0 // R value
       imageArray[i + 1] = 170// G value
       imageArray[i + 2] = 0 // B value
@@ -42,12 +47,14 @@ export class Triangle {
 
   draw (): void {
     this.resetContext()
+    this.context.fillStyle = 'white'
+    this.context.fillRect(0, 0, 1, 1)
     this.offscreenContext.putImageData(this.imageData, 0, 0)
     this.context.save()
     this.context.beginPath()
-    const x = 0.25
-    const y = 0.2
-    const size = 0.5
+    const x = this.drawX
+    const y = this.drawY
+    const size = this.drawSize
     const xMid = x + 0.5 * size
     const height = 0.5 * sqrt3 * size
     const halfPixel = 0.5 * size / Triangle.resolution
@@ -58,6 +65,20 @@ export class Triangle {
     this.context.clip()
     this.context.drawImage(this.offscreenCanvas, x, y, size, size)
     this.context.restore()
+    this.drawStrategy()
+  }
+
+  drawStrategy (): void {
+    if (this.strategy == null) return
+    this.resetContext()
+    const b = simplexToBox(this.strategy)
+    const x = this.drawX + this.drawSize * b.x
+    const y = this.drawX + this.drawSize * b.y
+    this.context.lineWidth = 0.005
+    this.context.strokeStyle = 'blue'
+    this.context.beginPath()
+    this.context.arc(x, y, 0.01, 0, 2 * Math.PI)
+    this.context.stroke()
   }
 
   resetContext (): void {
@@ -66,5 +87,18 @@ export class Triangle {
     this.context.scale(this.canvas.width, -this.canvas.height)
     this.context.globalAlpha = 1
     this.context.imageSmoothingEnabled = false
+  }
+
+  onMouseDown (event: MouseEvent): void {
+    const canvasX = event.offsetX / this.canvas.clientWidth
+    const canvasY = 1 - event.offsetY / this.canvas.clientHeight
+    const boxX = (canvasX - this.drawX) / this.drawSize
+    const boxY = (canvasY - this.drawY) / this.drawSize
+    const b = new Vec2(boxX, boxY)
+    const s = boxToSimplex(b)
+    if (Math.min(s.x, s.y, s.z) < -0.1) return
+    this.strategy = toInnerSimplex(boxToSimplex(b))
+    console.log(round(this.strategy.x, 2), round(this.strategy.y, 2), round(this.strategy.z, 2))
+    this.draw()
   }
 }
